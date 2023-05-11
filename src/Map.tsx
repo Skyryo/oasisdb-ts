365b6800364a8a0fbc969bdb3a15d921c9d48d0d
autumn1221 <autumn00129@gmail.com>
2023-05-11 19:23:26 +0900

    issue2,地図修正、マーカ追加

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,76 +1,57 @@
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-
-// import L from "leaflet";
-// import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
-// import markerIcon from "leaflet/dist/images/marker-icon.png";
-// import markerShadow from "leaflet/dist/images/marker-shadow.png";
-
-// delete L.Icon.Default.prototype._getIconUrl;
-// L.Icon.Default.mergeOptions({
-//   iconUrl: markerIcon.src,
-//   iconRetinaUrl: markerIcon2x.src,
-//   shadowUrl: markerShadow.src,
-// });
+import L from 'leaflet'
+L.Icon.Default.imagePath = 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/'
 
 const Map = () => {
   return (
+    <div>
     <MapContainer
-      center={[34.9843, 135.7596]}
-      zoom={2.5}
+      center={[30,0]}
+      zoom={2}
+      zoomSnap={0.1}
       scrollWheelZoom={false}
-      style={{ height: "100vh", width: "100%" }}
+      zoomControl={false}
+      dragging={false}
+      style={{ height: "100vh", width: "75%" }}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {/* <TileLayer
-        attribution='&copy; <a href="https://maps.gsi.go.jp/development/ichiran.html">地理院タイル</a>'
-        url="https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png"
-      /> */}
-      {/* <Marker position={[51.505, -0.09]}>
+      <Marker position={[35.6809591,139.7673068]}>
         <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
+          <a href="">日本</a>
         </Popup>
-      </Marker> */}
+      </Marker>
+      <Marker position={[37.566295,126.977945]}>
+        <Popup>
+        <a href="">韓国</a>
+        </Popup>
+      </Marker>
+      <Marker position={[39.904650,116.718487]}>
+        <Popup>
+        <a href="">中国</a>
+        </Popup>
+      </Marker>
+      <Marker position={[55.761389,37.608937]}>
+        <Popup>
+        <a href="">ロシア</a>
+        </Popup>
+      </Marker>
+      <Marker position={[38.895450,-77.015870]}>
+        <Popup>
+        <a href="">アメリカ</a>
+        </Popup>
+      </Marker>
+      <Marker position={[-35.310693,149.116694]}>
+        <Popup>
+        <a href="">オーストラリア</a>
+        </Popup>
+      </Marker>
     </MapContainer>
+    </div>
   );
 };
 
 export default Map;
-
-
-// import React, { useState } from 'react';
-// import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-
-// async function getCountryName(lat: number, lng: number): Promise<string> {
-//   const response = await fetch(`https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lng}&format=jsonv2`);
-//   const data = await response.json();
-//   return data.address.country;
-// }
-
-// function WorldMap() {
-//   const [position, setPosition] = useState<L.LatLngExpression | null>(null);
-//   const [countryName, setCountryName] = useState<string | null>(null);
-
-//   async function handleClick(e: L.LeafletMouseEvent) {
-//     const latlng = e.latlng;
-//     const countryName = await getCountryName(latlng.lat, latlng.lng);
-//     setPosition(latlng);
-//     setCountryName(countryName);
-//   }
-
-//   return (
-//     <MapContainer center={[0, 0]} zoom={3} onClick={handleClick}>
-//       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-//       {position && countryName && (
-//         <Marker position={position}>
-//           <Popup>{countryName}</Popup>
-//         </Marker>
-//       )}
-//     </MapContainer>
-//   );
-// }
-
-// export default WorldMap;
